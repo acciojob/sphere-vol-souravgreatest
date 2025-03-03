a6fa68a10ace637daace2d5156637d637991f1a2
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-03 18:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-  let rad=document.getElementById('radius').value;
-	if(rad<0 || isNaN(rad)){
-		document.getElementById('volume').value='NaN';
-		return;
-	}
-	let vol = (4/3) * Math.PI * Math.pow(rad, 3);
-	document.getElementById('volume').value=
-	vol.toFixed(4);
-} 
+function volume_sphere(e) {
+	e.preventDefault();
+    var radius = document.getElementById("radius").value;
+	console.log(radius)
+    if (isNaN(radius) || radius < 0) {
+	    document.getElementById("volume").value = 'NaN';
+        return;
+    }
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(4);
+}
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
